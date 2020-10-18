0dd944e944c6dda341f8417a421260880f39ff04
Sarah Fujimori <Sarah@Sarahs-MBP-2.attlocal.net>
2020-10-17 21:22:29 -0700

    Write BFS, DFS, shuffle method for testing; converted to tuple of tuples

--- puzzle.py
+++ puzzle.py
@@ -1,4 +1,17 @@
 import copy
+import random
+from time import sleep
+
+def memoize(function):
+  memo = {}
+  def wrapper(*args):
+    if args in memo:
+      return memo[args]
+    else:
+      rv = function(*args)
+      memo[args] = rv
+      return rv
+  return wrapper
 
 def LoadFromFile(filepath):
     with open(filepath, "r") as f:
@@ -14,7 +27,7 @@ def LoadFromFile(filepath):
             if len(row) != N:
                 print("Error"); return
             table.append(row)
-        return (table)
+        return tuple(tuple(row) for row in table)
 
 def DebugPrint(state):
     for row in state:
@@ -26,10 +39,10 @@ def DebugPrint(state):
 # Takes in state and positions of tiles to be swapped
 # Returns new state
 def swap_tiles(state, r1, c1, r2, c2):
-    new_state = copy.deepcopy(state)
+    new_state = list(list(row) for row in copy.deepcopy(state))
     new_state[r1][c1] = new_state[r2][c2]
     new_state[r2][c2] = state[r1][c1]
-    return new_state
+    return tuple(tuple(row) for row in new_state)
 
 def ComputeNeighbors(state):
     N = len(state)
@@ -64,39 +77,70 @@ def flatten(nested_list):
     return flatten_rec(nested_list)
 
 def IsGoal(state):
-    return flatten(state) == list(range(1, len(state)**2)) + [0]
+    return flatten(list(list(i) for i in state)) == list(range(1, len(state)**2)) + [0]
 
 def BFS(state):
     frontier = [(0, state)]
-    discovered = set(tuple(flatten(state)))
-    parents = {(0, tuple(flatten(state))): None}
+    discovered = set([state])
+    parents = {(0, state): None}
     path = []
     while len(frontier) != 0:
         current_state = frontier.pop(0)
-        discovered.add(tuple(flatten(current_state[1])))
+        discovered.add(current_state[1])
         if IsGoal(current_state[1]):
-            while parents.get((current_state[0], tuple(flatten(current_state[1])))) != None:
+            while parents.get((current_state[0], current_state[1])) != None:
                 path.insert(0, current_state[0])
-                current_state = parents.get((current_state[0], tuple(flatten(current_state[1]))))
+                current_state = parents.get((current_state[0], current_state[1]))
             return path
         for neighbor in ComputeNeighbors(current_state[1]):
-            if tuple(flatten(neighbor[1])) not in discovered:
+            # print(neighbor[0])
+            if neighbor[1] not in discovered:
                 frontier.append(neighbor)
-                discovered.add(tuple(flatten(neighbor[1])))
-                parents.update({(neighbor[0], tuple(flatten(neighbor[1]))): current_state})
+                discovered.add(neighbor[1])
+                parents.update({(neighbor[0], neighbor[1]): current_state})
     print("FAIL")
     return None
 
+def DFS(state):
+    frontier = [(0, state)]
+    discovered = set([state])
+    parents = {(0, state): None}
+    path = []
+    while len(frontier) != 0:
+        current_state = frontier.pop(0)
+        discovered.add(current_state[1])
+        if IsGoal(current_state[1]):
+            while parents.get((current_state[0], current_state[1])) != None:
+                path.insert(0, current_state[0])
+                current_state = parents.get((current_state[0], current_state[1]))
+            return path
+        for neighbor in ComputeNeighbors(current_state[1]):
+            if neighbor[1] not in discovered:
+                frontier.insert(0, neighbor)
+                discovered.add(neighbor[1])
+                parents.update({(neighbor[0], neighbor[1]): current_state})
+    print("FAIL")
+    return None
+
+# Method for creating test cases by randomly moving to a neighbor.
+def shuffle(state, steps=30):
+    current_state = state
+    for i in range(steps):
+        current_state = random.choice(ComputeNeighbors(current_state))[1]
+    return current_state
+
 table = LoadFromFile("test_puzzle.txt")
 DebugPrint(table)
+print(IsGoal(table))
 # print(20*"-")
-table = swap_tiles(table, 3, 3, 2, 2)
+# table = swap_tiles(table, 3, 3, 3, 2)
+# table = swap_tiles(table, 3, 2, 3, 0)
+table = shuffle(table)
 DebugPrint(table)
-# DebugPrint(table)
 # print("Neighbors: " + 40*"-")
 # n = ComputeNeighbors(table)
 # for i in n:
 #     print(i[0])
 #     DebugPrint(i[1])
 #     print(40*"-")
-print(BFS(table))
+print(DFS(table))
